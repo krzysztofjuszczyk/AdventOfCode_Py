ae3babd7668fa1e2b046da94f5fea724f6a074d9
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-09-15 13:29:54 +0200

    day 3 character set part B

--- 2022/day3.py
+++ 2022/day3.py
@@ -10,3 +10,13 @@ for line in lines:
     ans += ord(common_item) - ascii_start
 
 print('part A: '+ str(ans))
+
+lines2 = open('./inputs/d3.txt', 'r').read().splitlines()
+ans2 = 0
+groups = [[lines2[i], lines2[i + 1], lines2[i + 2]] for i, _ in enumerate(lines2[:-2]) if i % 3 == 0]
+for group in groups:
+    common_items_set = set(group[0]).intersection(group[1], group[2])
+    common_item = common_items_set.pop()
+    ascii_start = 96 if common_item.islower() else 38
+    ans2 += ord(common_item) - ascii_start
+print("Task 2 result: " + str(ans2))
