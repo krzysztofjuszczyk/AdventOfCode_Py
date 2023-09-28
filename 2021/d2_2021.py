c4961c0ebbbeaba06d56f427cf5cd2141681d115
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-09-28 09:09:40 +0200

    2021 day2 B

--- 2021/d2_2021.py
+++ 2021/d2_2021.py
@@ -1,7 +1,6 @@
 lines = open('./inputs/d2.txt', 'r').readlines()
 horizontal = 0
 depth = 0
-ans = 0
 for l in lines:
     cmd = l.split(' ')
     command = cmd[0]
@@ -12,3 +11,18 @@ for l in lines:
 
 print(horizontal*depth)
 
+# part B
+aim = 0
+horizontal = 0
+depth = 0
+for l in lines:
+    cmd = l.split(' ')
+    command = cmd[0]
+    steps = int(cmd[1])
+    if command == 'forward':
+        horizontal += steps
+        depth += (steps*aim)
+    elif command == 'down': aim += steps
+    else: aim -= steps
+
+print(horizontal*depth)
