21326bd14a956b85ea64831bafbdffce625b9207
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-09-29 14:49:20 +0200

    2021 day3 A

---
+++ 2021/d3_2021.py
@@ -0,0 +1,21 @@
+lines = open('./inputs/d3.txt', 'r').readlines()
+
+# initialize empty strings
+gamma, epsilon = '', ''
+
+for i in range (12):
+    count1, count0 = 0, 0
+    for l in lines:
+        if l[i] == '1': count1 += 1
+        else: count0 += 1
+    if count1>count0:
+        gamma += '1'
+        epsilon += '0'
+    else:
+        gamma += '0'
+        epsilon += '1'
+    print(gamma)
+    print(epsilon)
+
+ans = int(gamma,2)*int(epsilon,2)
+print(ans)
