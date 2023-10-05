c0dde5dab049b9ec1815c57f32b227d9dd5bbdd7
Krzysztof Juszczyk <100209790+krzysztofjuszczyk@users.noreply.github.com>
2023-10-05 15:43:10 +0200

    refactoring 2019 d1 recursive

---
+++ 2019_d1.py
@@ -0,0 +1,23 @@
+lines = open("./inputs/2019_d1.txt","r")
+ans = 0
+ansB = 0
+
+# part B
+def fuelRecursive(f):
+    calculated = 0
+    if f<=0:
+        return 0
+    else:
+        calculated = (f//3)-2 if (f//3)-2 >0 else 0
+        return calculated + fuelRecursive(calculated)
+
+
+print(fuelRecursive(100756))
+# part A
+for l in lines:
+    f = (int(l)//3)-2
+    ans += f
+    ansB += f + fuelRecursive(f)
+print(ans)
+print(ansB)
+
